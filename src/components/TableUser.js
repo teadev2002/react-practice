5f81f68bc417cafa9760a112684d8e0129b141f2
teadev2002 <anhhoanggg855@gmail.com>
2024-12-30 16:42:33 +0700

    paging

--- src/components/TableUser.js
+++ src/components/TableUser.js
@@ -2,49 +2,58 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Table from "react-bootstrap/Table";
 import { fetchAllUser } from "../services/UserService"; // đóng ngoac do export dưới dạng obj
+import ReactPaginate from "react-paginate";
+
 const TableUsers = (props) => {
   const [listUsers, setListUsers] = useState([]);
-
-  const getUsers = async () => {
-    // async trước hàm
-
-    let res = await fetchAllUser();
-
-    console.log("check new res:", res);
-
+  const [totalUsers, setTotalUsers] = useState(0); // paging: ban đầu ko có dữ liệu thì là 0
+  const [totalPages, setTotalPages] = useState(0); // tổng số trang
+  const getUsers = async (page) => {
+    let res = await fetchAllUser(page);
     if (res && res.data && res.data.data) {
       setListUsers(res.data.data);
+      setTotalUsers(res.data.total); // set tổng số user
+      setTotalPages(res.data.total_pages); // set tổng số trang
     }
-
-    console.log(listUsers);
   };
 
   useEffect(() => {
+    // call api
+    // dry
     getUsers();
   }, []);
 
-  useEffect(() => {
-    console.log("Danh sách người dùng:", listUsers);
-  }, [listUsers]);
+  useEffect(() => {}, [listUsers]);
 
+  // hàm xử lí mỗi lần click chuyển trang sẽ gọi api lấy đúng số lượng user trong trang
+  const handlePageClick = (event) => {
+    getUsers(+event.selected + 1);
+  };
   return (
     <>
       <Table striped bordered hover>
-        <thead>
+        <thead style={{ textAlign: "center" }}>
           <tr>
             <th>ID</th>
+            <th>Avatar</th>
             <th>Email</th>
             <th>First Name</th>
             <th>Last Name</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody style={{ textAlign: "center" }}>
           {listUsers &&
             listUsers.length > 0 &&
             listUsers.map((item, index) => {
               return (
                 <tr key={`users-${index}`}>
                   <td> {item.id}</td>
+                  <td>
+                    <img
+                      style={{ borderRadius: 100, width: 40 }}
+                      src={item.avatar}
+                    />
+                  </td>
                   <td>{item.email}</td>
                   <td>{item.first_name}</td>
                   <td>{item.last_name}</td>
@@ -53,6 +62,25 @@ const TableUsers = (props) => {
             })}
         </tbody>
       </Table>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={totalPages}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+      />
     </>
   );
 };
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,7 +1,7 @@
 import axios from "./my-axios";
-
-const fetchAllUser = () => {
-  return axios.get(`/api/users?page=1`); // this is promise
+// truyền vào danh sách người dùng đang ở trang bao nhiêu
+const fetchAllUser = (page) => {
+  return axios.get(`/api/users?page=${page}`); // this is promise
 };
 
 // export dưới dạng obj thì export bao nhiêu biến cũng được
